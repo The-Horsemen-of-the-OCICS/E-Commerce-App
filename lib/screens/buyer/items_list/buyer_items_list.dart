9e6db47f6365ff603dbd449bf56675f1844889f0
Kobe Dai <kobedai24@gmail.com>
2022-02-08 21:21:34 -0500

    【Feature】update mock data
    1. update mock data

--- lib/screens/buyer/items_list/buyer_items_list.dart
+++ lib/screens/buyer/items_list/buyer_items_list.dart
@@ -41,6 +41,14 @@ class _BuyerItemsListState extends State<BuyerItemsList> {
       Item('Women Cloth', 'Women cloth desc', 50, 'https://i.postimg.cc/2yMqQ5Cd/1624937261e1565ed7bb7611d917ff2e6a9ffe580a-thumbnail-900x.jpg'),
       Item('Kids Cloth', 'Kids cloth desc', 80, 'https://i.postimg.cc/d10DgC1m/16172552205a1794e7dc17db68856850f0c26eeb53-thumbnail-900x.jpg'), 
       Item('Home products', 'Home products desc', 120, 'https://i.postimg.cc/j5kCTjnV/16340030929e7b3bd5c75857d1c040c639acc70476-thumbnail-900x.jpg'),
+      Item('Men Cloth', 'Men cloth desc', 100, 'https://i.postimg.cc/Pr0ZZSxG/1641969100f69da7264d8688d9c11e7ce8cd3597b0-thumbnail-900x.jpg'), 
+      Item('Women Cloth', 'Women cloth desc', 50, 'https://i.postimg.cc/2yMqQ5Cd/1624937261e1565ed7bb7611d917ff2e6a9ffe580a-thumbnail-900x.jpg'),
+      Item('Kids Cloth', 'Kids cloth desc', 80, 'https://i.postimg.cc/d10DgC1m/16172552205a1794e7dc17db68856850f0c26eeb53-thumbnail-900x.jpg'), 
+      Item('Home products', 'Home products desc', 120, 'https://i.postimg.cc/j5kCTjnV/16340030929e7b3bd5c75857d1c040c639acc70476-thumbnail-900x.jpg'),
+      Item('Men Cloth', 'Men cloth desc', 100, 'https://i.postimg.cc/Pr0ZZSxG/1641969100f69da7264d8688d9c11e7ce8cd3597b0-thumbnail-900x.jpg'), 
+      Item('Women Cloth', 'Women cloth desc', 50, 'https://i.postimg.cc/2yMqQ5Cd/1624937261e1565ed7bb7611d917ff2e6a9ffe580a-thumbnail-900x.jpg'),
+      Item('Kids Cloth', 'Kids cloth desc', 80, 'https://i.postimg.cc/d10DgC1m/16172552205a1794e7dc17db68856850f0c26eeb53-thumbnail-900x.jpg'), 
+      Item('Home products', 'Home products desc', 120, 'https://i.postimg.cc/j5kCTjnV/16340030929e7b3bd5c75857d1c040c639acc70476-thumbnail-900x.jpg'),
     ];
     await Future.delayed(const Duration(seconds: 2));
 
