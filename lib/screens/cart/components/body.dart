524c0ac81e55bc001dd38c8cae3463f93d325a9f
Rui Cai <rui.cai@wesleyclover.com>
2022-03-08 22:09:44 -0500

    UI update

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ecommerceapp/models/cart.dart';
 import 'cart_card.dart';
-import 'cart_cart1.dart';
 import '../../../size_config.dart';
 
 class Body extends StatefulWidget {
