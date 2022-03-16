a96270e3a12e05883b1ff5e7a707c80c5ac086a1
Yukang Yin <yukangyin@outlook.com>
2022-03-16 10:43:08 -0400

    add item category by API

--- lib/screens/merchant/merchant_category_list.dart
+++ lib/screens/merchant/merchant_category_list.dart
@@ -6,6 +6,7 @@ import 'package:ecommerceapp/widgets/merchant_category_cell.dart';
 import 'package:http/http.dart' as http;
 import 'package:ecommerceapp/utils/network_config.dart';
 import 'dart:convert';
+import 'dart:math';
 
 class MerchantCategoryList extends StatefulWidget {
   const MerchantCategoryList({Key? key}) : super(key: key);
@@ -36,6 +37,23 @@ class _MerchantCategoryListState extends State<MerchantCategoryList> {
   Future<void> deleteCategoryById(http.Client client, int id) async {
     await client.delete(
         Uri.parse(NetworkConfig.API_BASE_URL + 'category/' + id.toString()));
+  }
+
+  Future<ItemCategory> createItemCategory(
+      http.Client client, ItemCategory itemCategory) async {
+    final response = await client.post(
+      Uri.parse(NetworkConfig.API_BASE_URL + 'category/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'id': itemCategory.id.toString(),
+        'name': itemCategory.name,
+        'icon': itemCategory.icon
+      }),
+    );
+
+    return ItemCategory.fromJson(jsonDecode(response.body));
   }
 
   TextEditingController _name = TextEditingController(text: "");
@@ -96,9 +114,14 @@ class _MerchantCategoryListState extends State<MerchantCategoryList> {
               borderRadius: BorderRadius.all(Radius.circular(2)),
             )),
         onPressed: () {
-          setState(() {
-            _categories.add(ItemCategory(
-                id: _categories.length, name: _name.text, icon: _icon.text));
+          ItemCategory itemCategory = ItemCategory(
+              id: Random().nextInt(999999), name: _name.text, icon: _icon.text);
+
+          createItemCategory(http.Client(), itemCategory)
+              .then((createdItemCategory) {
+            setState(() {
+              _categories.add(createdItemCategory);
+            });
           });
         },
         child: const Text('Submit',
