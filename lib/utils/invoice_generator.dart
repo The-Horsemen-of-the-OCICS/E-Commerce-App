6a7e8f5c1038282204787f3436e2d4a970154af8
Rui Cai <rui.cai@wesleyclover.com>
2022-03-18 22:20:42 -0400

    add invoice pdf downloader

---
+++ lib/utils/invoice_generator.dart
@@ -0,0 +1,223 @@
+import 'dart:io';
+import './pdf_api.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:pdf/widgets.dart';
+import '../models/order.dart';
+
+class InvoiceGenerator {
+  static Future<File> generate(Order order) async {
+    final pdf = Document();
+
+    pdf.addPage(MultiPage(
+      pageFormat: PdfPageFormat.a4,
+      build: (Context context) => [
+        builderForHeader(order),
+        SizedBox(height: 3 * PdfPageFormat.cm),
+        builderForTitle(order),
+        builderForInvoice(order),
+        Divider(),
+        builderForTotal(order),
+      ],
+      footer: (context) => builderForFooter(),
+    ));
+
+    return PdfApi.saveDocument(name: order.id.toString(), pdf: pdf);
+  }
+
+  static Widget builderForHeader(Order order) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 1 * PdfPageFormat.cm),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              buildSupplierAddress('75 Rue de la République, 75018 Paris'),
+              Container(
+                height: 50,
+                width: 50,
+                child: BarcodeWidget(
+                  barcode: Barcode.qrCode(),
+                  data: order.id,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 1 * PdfPageFormat.cm),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              buildCustomerAddress(order),
+              buildInvoiceInfo(order),
+            ],
+          ),
+        ],
+      );
+
+  static Widget buildCustomerAddress(Order order) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(order.userId),
+          Text(order.shippingAddress),
+        ],
+      );
+
+  static Widget buildInvoiceInfo(Order order) {
+    final titles = <String>[
+      'Invoice Number:',
+      'Invoice Date:',
+      'Payment Terms:',
+    ];
+    final paymentMethods = 'Online Credit Card';
+    final data = <String>[
+      order.id,
+      order.orderDate,
+      paymentMethods,
+    ];
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: List.generate(titles.length, (index) {
+        final title = titles[index];
+        final value = data[index];
+
+        return buildText(title: title, value: value, width: 200);
+      }),
+    );
+  }
+
+  static Widget buildSupplierAddress(String address) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Horseman. Co LTD.',
+              style: TextStyle(fontWeight: FontWeight.bold)),
+          SizedBox(height: 1 * PdfPageFormat.mm),
+          Text(address),
+        ],
+      );
+
+  static Widget builderForTitle(Order order) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'INVOICE',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 0.8 * PdfPageFormat.cm),
+          Text('Invoice shown below is a electronically generated invoice.'),
+          SizedBox(height: 0.8 * PdfPageFormat.cm),
+        ],
+      );
+
+  static Widget builderForInvoice(Order order) {
+    final headers = ['Item Name', 'Quantity', 'Unit Price', 'SubTotal'];
+    final data = order.cartList.map((cartItem) {
+      final subTotal = cartItem.itemPrice * cartItem.quantity;
+
+      return [
+        cartItem.name,
+        '${cartItem.quantity}',
+        '\$ ${cartItem.itemPrice}',
+        '\$ ${subTotal.toStringAsFixed(2)}',
+      ];
+    }).toList();
+
+    return Table.fromTextArray(
+      headers: headers,
+      data: data,
+      border: null,
+      headerStyle: TextStyle(fontWeight: FontWeight.bold),
+      headerDecoration: BoxDecoration(color: PdfColors.grey300),
+      cellHeight: 30,
+      cellAlignments: {
+        0: Alignment.centerLeft,
+        1: Alignment.centerRight,
+        2: Alignment.centerRight,
+        3: Alignment.centerRight,
+        4: Alignment.centerRight,
+        5: Alignment.centerRight,
+      },
+    );
+  }
+
+  static Widget builderForTotal(Order order) {
+    final netTotal = order.cartList
+        .map((cartItem) => cartItem.itemPrice * cartItem.quantity)
+        .reduce((item1, item2) => item1 + item2);
+    final taxRate = 0.13;
+    final tax = netTotal * taxRate;
+    final total = netTotal + tax;
+
+    return Container(
+      alignment: Alignment.centerRight,
+      child: Row(
+        children: [
+          Spacer(flex: 6),
+          Expanded(
+            flex: 4,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildText(
+                  title: 'Net total',
+                  value: '\$ ${netTotal.toStringAsFixed(2)}',
+                  unite: true,
+                ),
+                buildText(
+                  title: 'Vat ${taxRate * 100} %',
+                  value: '\$ ${tax.toStringAsFixed(2)}',
+                  unite: true,
+                ),
+                Divider(),
+                buildText(
+                  title: 'Total amount',
+                  titleStyle: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  value: '\$ ${total.toStringAsFixed(2)}',
+                  unite: true,
+                ),
+                SizedBox(height: 2 * PdfPageFormat.mm),
+                Container(height: 1, color: PdfColors.grey400),
+                SizedBox(height: 0.5 * PdfPageFormat.mm),
+                Container(height: 1, color: PdfColors.grey400),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  static Widget builderForFooter() => Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Divider(),
+          SizedBox(height: 2 * PdfPageFormat.mm),
+          Text('Thanks for your order!',
+              style: TextStyle(fontWeight: FontWeight.bold)),
+        ],
+      );
+
+  static buildText({
+    required String title,
+    required String value,
+    double width = double.infinity,
+    TextStyle? titleStyle,
+    bool unite = false,
+  }) {
+    final style = titleStyle ?? TextStyle(fontWeight: FontWeight.bold);
+
+    return Container(
+      width: width,
+      child: Row(
+        children: [
+          Expanded(child: Text(title, style: style)),
+          Text(value, style: unite ? style : null),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/order_history_item.dart
+++ lib/widgets/order_history_item.dart
@@ -1,6 +1,7 @@
+import 'package:ecommerceapp/utils/invoice_generator.dart';
+import 'package:ecommerceapp/utils/pdf_api.dart';
 import 'package:flutter/material.dart';
 import '../models/order.dart';
-import '../screens/forum/question_detail_screen.dart';
 
 class OrderHistroyItem extends StatefulWidget {
   const OrderHistroyItem({Key? key, required this.order}) : super(key: key);
@@ -14,102 +15,122 @@ class OrderHistroyItem extends StatefulWidget {
 class _OrderHistroyItemState extends State<OrderHistroyItem> {
   @override
   Widget build(BuildContext context) {
+    Widget downloadInvoice = Container(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 10),
+        child: IconButton(
+            icon: Icon(Icons.download),
+            onPressed: () async {
+              print('xxxx');
+              final pdfFile = await InvoiceGenerator.generate(widget.order);
+              PdfApi.openFile(pdfFile);
+            }),
+      ),
+    );
+
     return Column(children: [
       GestureDetector(
           key: const Key('order_history_item'),
           onTap: () {},
           child: Container(
-            constraints: const BoxConstraints(
-              maxWidth: 600,
-            ),
-            margin: EdgeInsets.all(2.0),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black26.withOpacity(0.1),
-                      offset: Offset(0.0, 6.0),
-                      blurRadius: 10.0,
-                      spreadRadius: 0.10)
-                ]),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Order # ${widget.order.id}",
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: .4),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Row(
-                                children: <Widget>[
-                                  Text(
-                                    "${widget.order.orderDate}",
-                                    style: TextStyle(color: Colors.grey),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, left: 10),
-                    child: Text(
-                      "Shipped To: ${widget.order.shippingAddress}",
-                      maxLines: 3,
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                          color: Colors.black.withOpacity(0.6),
-                          fontSize: 18,
-                          letterSpacing: .2),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                      "Total Price: ${widget.order.overallPrice.toString()}",
-                      maxLines: 3,
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                          color: Colors.black.withOpacity(0.6),
-                          fontSize: 18,
-                          letterSpacing: .2),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 10),
-                    child: Text(
-                      "Items: ${widget.order.printItems()}",
-                      maxLines: 3,
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                          color: Colors.black.withOpacity(0.6),
-                          fontSize: 18,
-                          letterSpacing: .2),
-                    ),
-                  )
-                ],
+              constraints: const BoxConstraints(
+                maxWidth: 600,
               ),
-            ),
-          ))
+              margin: EdgeInsets.all(2.0),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black26.withOpacity(0.1),
+                        offset: Offset(0.0, 6.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.10)
+                  ]),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: (Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Order ${widget.order.id}",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: .4),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Row(
+                                        children: <Widget>[
+                                          Text(
+                                            "${widget.order.orderDate}",
+                                            style:
+                                                TextStyle(color: Colors.grey),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Text(
+                              "Shipped To: ${widget.order.shippingAddress}",
+                              maxLines: 3,
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  color: Colors.black.withOpacity(0.6),
+                                  fontSize: 18,
+                                  letterSpacing: .2),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 0),
+                            child: Text(
+                              "Total Price: ${widget.order.overallPrice.toString()}",
+                              maxLines: 3,
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  color: Colors.black.withOpacity(0.6),
+                                  fontSize: 18,
+                                  letterSpacing: .2),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              "Items: ${widget.order.printItems()}",
+                              maxLines: 3,
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  color: Colors.black.withOpacity(0.6),
+                                  fontSize: 18,
+                                  letterSpacing: .2),
+                            ),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[downloadInvoice],
+                      )
+                    ])),
+              )))
     ]);
   }
 }
