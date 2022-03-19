92938f5a8207fa9a053264e3b2012236212027dc
Rui Cai <rui.cai@wesleyclover.com>
2022-03-18 22:47:49 -0400

    clean up

--- lib/utils/invoice_generator.dart
+++ lib/utils/invoice_generator.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
 import 'package:ecommerceapp/utils/save_to_file.dart';
-
 import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
 import 'package:pdf/widgets.dart';
 import '../models/order.dart';
 
