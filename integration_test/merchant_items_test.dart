9f4e65dd1adb9561db0070f0a88ee6b40b17b8e9
Yukang Yin <yukangyin@outlook.com>
2022-03-20 18:26:02 -0400

    add_merchant_items_integration_test

--- integration_test/merchant_items_test.dart
+++ integration_test/merchant_items_test.dart
@@ -9,44 +9,44 @@ import 'package:ecommerceapp/main.dart' as app;
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   group('Merchant Items', () {
-    // testWidgets('As a merchant, I can view a list of items',
-    //     (WidgetTester tester) async {
-    //   app.main();
-    //   await tester.pumpAndSettle();
+    testWidgets('As a merchant, I can view a list of items',
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
 
-    //   // find login button on home page
-    //   final homeLoginButtonFind = find.byKey(const Key("home_login_button"));
-    //   expect(homeLoginButtonFind, findsOneWidget);
+      // find login button on home page
+      final homeLoginButtonFind = find.byKey(const Key("home_login_button"));
+      expect(homeLoginButtonFind, findsOneWidget);
 
-    //   // tap and enter login page
-    //   await tester.tap(homeLoginButtonFind);
-    //   await tester.pumpAndSettle();
+      // tap and enter login page
+      await tester.tap(homeLoginButtonFind);
+      await tester.pumpAndSettle();
 
-    //   // find two input fields
-    //   final textFormsFind = find.byType(TextFormField);
-    //   expect(textFormsFind, findsNWidgets(2));
+      // find two input fields
+      final textFormsFind = find.byType(TextFormField);
+      expect(textFormsFind, findsNWidgets(2));
 
-    //   final emailFormFind = textFormsFind.first;
-    //   final passwordFormFind = textFormsFind.last;
+      final emailFormFind = textFormsFind.first;
+      final passwordFormFind = textFormsFind.last;
 
-    //   await tester.enterText(emailFormFind, 'admin@gmail.com');
-    //   await tester.pumpAndSettle();
-    //   await tester.enterText(passwordFormFind, '123456');
-    //   await tester.pumpAndSettle();
+      await tester.enterText(emailFormFind, 'admin@gmail.com');
+      await tester.pumpAndSettle();
+      await tester.enterText(passwordFormFind, '123456');
+      await tester.pumpAndSettle();
 
-    //   // find the login button
-    //   final loginButtonFind = find.byType(ElevatedButton);
-    //   expect(loginButtonFind, findsOneWidget);
+      // find the login button
+      final loginButtonFind = find.byType(ElevatedButton);
+      expect(loginButtonFind, findsOneWidget);
 
-    //   // try login
-    //   await tester.tap(find.byType(ElevatedButton).last);
-    //   await tester.pumpAndSettle();
+      // try login
+      await tester.tap(find.byType(ElevatedButton).last);
+      await tester.pumpAndSettle();
 
-    //   // enter merchant items page
-    //   NavigatorState navigator = tester.state(find.byType(Navigator));
-    //   navigator.pushNamed(AppRoutes.merchantItems);
-    //   await tester.pumpAndSettle();
-    // });
+      // enter merchant items page
+      NavigatorState navigator = tester.state(find.byType(Navigator));
+      navigator.pushNamed(AppRoutes.merchantItems);
+      await tester.pumpAndSettle();
+    });
 
     testWidgets('As a merchant, I can add a new item',
         (WidgetTester tester) async {
