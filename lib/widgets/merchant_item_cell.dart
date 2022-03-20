e7b4494ff0c61970cef4216d46d56a82719360a5
Yukang Yin <yukangyin@outlook.com>
2022-03-20 19:06:56 -0400

    Update delete merchant item integration test (#48)
    * update and delete merchant item integration test
    
    * update and delete merchant item integration test

--- lib/widgets/merchant_item_cell.dart
+++ lib/widgets/merchant_item_cell.dart
@@ -30,6 +30,7 @@ class _MerchantItemCellState extends State<MerchantItemCell> {
   @override
   Widget build(BuildContext context) {
     final deleteItemButton = InkWell(
+        key: const Key('delete_item_key'),
         child: Container(
           child: const Icon(
             Icons.delete,
@@ -40,6 +41,7 @@ class _MerchantItemCellState extends State<MerchantItemCell> {
           widget.removeItem(widget.item);
         });
     final editItemButton = InkWell(
+        key: const Key('edit_item_key'),
         child: Container(
           child: const Icon(
             Icons.edit,
